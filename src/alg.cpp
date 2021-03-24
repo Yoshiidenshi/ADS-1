af290738a9034bd47663dbb4a49c341dc31b3456
Евгений Бажин <76097199+Yoshiidenshi@users.noreply.github.com>
2021-03-24 15:51:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,14 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int right, left, count, mid;
+  int right, left, count;
   right = size - 1; 
   left = 0;
   count = 0;
+  
   while (left < right) 
   {
-      mid = (right + left) / 2;
+      int mid = (right + left) / 2;
       if (arr[mid] < value)
           left = mid + 1;
       else
